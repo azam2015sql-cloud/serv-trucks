0524b74ea500b6012a53cf55a546a81228524253
azam2015sql-cloud <azam2015sql@gmail.com>
2025-08-06 14:03:51 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,25 +8,45 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 // Middleware لخدمة الملفات الثابتة
-app.use(express.static(path.join(__dirname, '../client')));
-app.use('/css', express.static(path.join(__dirname, '../client/css')));
-app.use('/js', express.static(path.join(__dirname, '../client/js')));
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.json());
 
 // جميع الطلبات ترجع index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
+
+// تخزين حالة الوحدات على السيرفر
+let workshopUnits = {};
 
 // أحداث Socket.io
 io.on('connection', (socket) => {
-    console.log('مستخدم جديد متصل');
-    
-    socket.on('disconnect', () => {
-        console.log('مستخدم قطع الاتصال');
-    });
+  console.log('مستخدم جديد متصل');
+  
+  // إرسال البيانات الأولية عند الاتصال
+  socket.emit('initialData', workshopUnits);
+  
+  // استقبال تحديثات الوحدات
+  socket.on('updateUnit', (unitData) => {
+    workshopUnits[unitData.unitNumber] = {
+      section: unitData.section,
+      lastMoveTime: unitData.lastMoveTime,
+      engineer: unitData.engineer
+    };
+    io.emit('unitUpdated', unitData);
+  });
+  
+  // طلب البيانات الحالية
+  socket.on('requestData', () => {
+    socket.emit('initialData', workshopUnits);
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('مستخدم قطع الاتصال');
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`الخادم يعمل على المنفذ ${PORT}`);
+  console.log(`الخادم يعمل على المنفذ ${PORT}`);
 });
