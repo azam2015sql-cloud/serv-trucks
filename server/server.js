c5369396ebb3322b24b8785021a542f92e085cf9
azam2015sql-cloud <azam2015sql@gmail.com>
2025-08-06 11:24:16 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// تحديد مسار الملفات الثابتة
+// مسار الملفات الثابتة
 const clientPath = path.join(__dirname, '../client');
 
 // Middleware لخدمة الملفات الثابتة
@@ -15,19 +15,19 @@ app.use(express.static(clientPath));
 
 // جميع الطلبات ترجع index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(clientPath, 'index.html'));
+  res.sendFile(path.join(clientPath, 'index.html'));
 });
 
 // أحداث Socket.io
 io.on('connection', (socket) => {
-    console.log('مستخدم جديد متصل');
-    
-    socket.on('disconnect', () => {
-        console.log('مستخدم قطع الاتصال');
-    });
+  console.log('مستخدم جديد متصل');
+  
+  socket.on('disconnect', () => {
+    console.log('مستخدم قطع الاتصال');
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`الخادم يعمل على المنفذ ${PORT}`);
+  console.log(`الخادم يعمل على المنفذ ${PORT}`);
 });
